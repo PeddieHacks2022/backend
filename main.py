6209a44f86199bed90705406b885205a6c6e2d27
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2022-08-19 23:50:35 -0500

    Added sqlite and DAO setup (#3)

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+# Imports
+import DAOs
 from flask import Flask
 from Account.signup import signup_blueprint
 
 def intializeBackend():
+    # Connect database 
+    DAOs.connectToDatabase("FitFormV1")
+
+    # Connect flask
     app = Flask(__name__)
     app.register_blueprint(signup_blueprint)
-    return app
+    app.run()
 
 if __name__ == "__main__":
-    app = intializeBackend()
-    app.run()
+    intializeBackend()
