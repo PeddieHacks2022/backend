6ec5dac3952e91e4d2e583931f34d1f3fa8c2359
Daniel Liu <mr.picklepinosaur@gmail.com>
2022-08-20 21:34:58 -0400

    Refactor routine POST endpoint to take list of workouts

--- User/workout_set.py
+++ User/workout_set.py
@@ -11,17 +11,23 @@ PREFIX = "/user/<int:user_id>/routine"
 def post(user_id: int):
 
     name = request.json.get("name", None)
+    workoutIDs = request.json.get("workoutIDs", None)
 
-    if not (name and type(name) == str):
+    if not (name and type(name) == str and workoutIDs and isinstance(workoutIDs, list)):
         return "Malformed Request", 400
 
-    model.create(user_id, name)
+    id = model.create(user_id, name)
+    for workoutID in workoutIDs:
+        model.insert_link(id, workoutID)
 
     return "Ok", 200
 
 @routine_blueprint.route(f"{PREFIX}/<int:routine_id>", methods=["PATCH"])
 def patch(user_id: int, routine_id: int):
 
+    return "Deprecated", 301
+
+    '''
     workout_id = request.json.get("workout_id", None)
 
     if not (workout_id and type(workout_id) == int):
@@ -31,6 +37,7 @@ def patch(user_id: int, routine_id: int):
     model.insert_link(routine_id, workout_id)
 
     return "Ok", 200
+    '''
 
 @routine_blueprint.route(PREFIX, methods=["GET"])
 def get(user_id: int):
